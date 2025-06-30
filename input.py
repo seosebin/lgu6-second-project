095b0eecd42167f22a1c609f2d4127d515474153
oys1205 <yosep.ciafirst@gmail.com>
2025-06-30 14:32:18 +0900

    updated

--- input.py
+++ input.py
@@ -34,6 +34,86 @@ loaded_model = joblib.load('predict_model/best_disease_model.joblib')
 label_encoder = joblib.load('predict_model/label_encoder.joblib')
 medicine_df = pd.read_csv('data/drug_info.csv')
 
+disease_translation = {
+    'Influenza': '독감',
+    'Asthma': '천식',
+    'Eczema': '습진',
+    'Depression': '우울증',
+    'Liver Cancer': '간암',
+    'Stroke': '뇌졸중',
+    'Urinary Tract Infection': '요로감염',
+    'Bipolar Disorder': '조울증',
+    'Bronchitis': '기관지염',
+    'Cerebral Palsy': '뇌성마비',
+    'Colorectal Cancer': '대장암',
+    'Hypertensive Heart Disease': '고혈압성 심장병',
+    'Multiple Sclerosis': '다발성 경화증',
+    'Myocardial Infarction (Heart...': '심근경색증',
+    'Urinary Tract Infection (UTI)': '요로감염(UTI)',
+    'Common Cold': '감기',
+    'Migraine': '편두통',
+    'Pneumonia': '폐렴',
+    'Cirrhosis': '간경변증',
+    'Conjunctivitis (Pink Eye)': '결막염',
+    'Gastroenteritis': '위장염',
+    'Hyperthyroidism': '갑상선기능항진증',
+    'Kidney Cancer': '신장암',
+    'Liver Disease': '간질환',
+    'Malaria': '말라리아',
+    'Pancreatitis': '췌장염',
+    'Rheumatoid Arthritis': '류마티스 관절염',
+    'Spina Bifida': '척추 이분증',
+    'Ulcerative Colitis': '궤양성 대장염',
+    'Anxiety Disorders': '불안장애',
+    'Diabetes': '당뇨병',
+    'Osteoarthritis': '골관절염',
+    'Klinefelter Syndrome': '클라인펠터 증후군',
+    'Chickenpox': '수두',
+    'Coronary Artery Disease': '관상동맥질환',
+    'Eating Disorders (Anorexia,...': '섭식장애(거식증 등)',
+    'Fibromyalgia': '섬유근육통',
+    'Hemophilia': '혈우병',
+    'Hypoglycemia': '저혈당증',
+    'Lymphoma': '림프종',
+    'Psoriasis': '건선',
+    'Tuberculosis': '결핵',
+    'Hypothyroidism': '갑상선기능저하증',
+    'Kidney Disease': '신장질환',
+    'Allergic Rhinitis': '알레르기 비염',
+    'Cataracts': '백내장',
+    "Crohn's Disease": '크론병',
+    'Hypertension': '고혈압',
+    'Osteoporosis': '골다공증',
+    'Pneumocystis Pneumonia (PCP)': '폐포자충 폐렴',
+    'Scoliosis': '척추측만증',
+    'Sickle Cell Anemia': '겸상 적혈구 빈혈',
+    'Tetanus': '파상풍',
+    'Down Syndrome': '다운 증후군',
+    'Ebola Virus': '에볼라 바이러스',
+    'Lyme Disease': '라임병',
+    'Pancreatic Cancer': '췌장암',
+    'Pneumothorax': '기흉',
+    'Hemorrhoids': '치질',
+    'Polycystic Ovary Syndrome (PCOS)': '다낭성 난소 증후군',
+    'Systemic Lupus Erythematosus...': '전신홍반루푸스',
+    'Typhoid Fever': '장티푸스',
+    'Chronic Kidney Disease': '만성 신장질환',
+    'Hepatitis B': 'B형 간염',
+    "Parkinson's Disease": '파킨슨병',
+    'Prader-Willi Syndrome': '프래더-윌리 증후군',
+    'Thyroid Cancer': '갑상선암',
+    "Alzheimer's Disease": '알츠하이머병',
+    'Chronic Obstructive Pulmonary Disease (COPD)': '만성폐쇄성폐질환(COPD)',
+    'Dementia': '치매',
+    'Diverticulitis': '게실염',
+    'Lung Cancer': '폐암',
+    'Mumps': '유행성이하선염(볼거리)',
+    'Gout': '통풍',
+    'Testicular Cancer': '고환암',
+    'Tonsillitis': '편도염',
+    'Williams Syndrome': '윌리엄스 증후군'
+}
+
 st.title('증상 입력 및 질병 예측')
 
 create_user_symptoms_table()
@@ -108,8 +188,12 @@ if st.button("선택하기"):
         new_patient_df = pd.DataFrame([new_patient])
         predicted_class = loaded_model.predict(new_patient_df)[0]
         predicted_label = label_encoder.inverse_transform([predicted_class])[0]
+        
+        predicted_label_kor = disease_translation.get(predicted_label, predicted_label)
 
-        st.success(f"예측된 질병: {predicted_label}")
+    
+
+        st.success(f"예측된 질병: {predicted_label_kor}")
 
         def symptom_match(efficacy_text):
             if pd.isna(efficacy_text):
@@ -147,7 +231,8 @@ if st.button("선택하기"):
             blood_pressure=blood_pressure,
             cholesterol=cholesterol
         )
-
+        
+        
         # insert_user_details(
         #     user_id=user_id,
         #     symptoms=", ".join(option),
